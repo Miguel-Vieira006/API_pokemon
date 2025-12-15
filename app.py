ebc410da2221e9d3f56236bff925bb7d67f55483
Miguel-Vieira006 <miguelvieiraxv@gmail.com>
2025-12-14 22:05:33 -0300

    feat: implementado metodo PUT /iniciais/<int:id_iniciais>

--- app.py
+++ app.py
@@ -176,31 +176,35 @@ def get_iniciais():
 # GET /iniciais/1
 @app.get('/iniciais/<int:id>')
 def get_iniciais_id(id):
-    for inicial in iniciais: 
-        if iniciais["id"] == id:
-            return jsonify(iniciais), 200
+    for inicial in iniciais:
+        if inicial["id"] == id:
+            return jsonify(inicial), 200
     return jsonify({"erro": "Iniciais não encontrados"}), 404
 
 # GET /iniciais?nome=XPTO
 @app.get('/iniciais/nome')
-def get_loja_name():
+def get_iniciais_nome():
     nome = request.args.get('nome')
+    if not nome:
+        return jsonify({"erro": "Parâmetro 'nome' é obrigatório"}), 400
     for inicial in iniciais:
-        if iniciais['nome'] == nome:
-            return jsonify(iniciais), 200
+        if inicial['nome'] == nome:
+            return jsonify(inicial), 200
     return jsonify({"erro": "Iniciais não encontrados"}), 404
 
-# POST /loja
-@app.post('/Iniciais')
+# POST /iniciais
+@app.post('/iniciais')
 def criar_iniciais():
     dado = request.get_json()
-
     if not dado: 
         return jsonify({"erro": "Dado não enviado"}), 400
     if "nome" not in dado: 
         return jsonify({"erro": "O campo 'nome' é obrigatório"}), 400
     if "id" not in dado: 
         return jsonify({"erro": "O campo 'id' é obrigatório"}), 400
+
+    if any(item['id'] == dado['id'] for item in iniciais):
+        return jsonify({"erro": "ID já existe"}), 400
 
     novos_iniciais = {
         "id": dado['id'],
@@ -211,6 +215,22 @@ def criar_iniciais():
     iniciais.append(novos_iniciais)
     return jsonify(novos_iniciais), 201
 
+# PUT /iniciais/<id_iniciais>
+@app.put('/iniciais/<int:id_iniciais>')
+def atualizar_iniciais(id_iniciais):
+    dado_novo = request.get_json()
+    if not dado_novo:
+        return jsonify({"erro": "Dado não enviado"}), 400
+    if 'nome' not in dado_novo: 
+        return jsonify({"erro": "O campo 'nome' é obrigatório"}), 400
+    
+    for inicial in iniciais:
+        if inicial["id"] == id_iniciais:
+            inicial['nome'] = dado_novo['nome']
+            inicial['items'] = dado_novo.get('items', inicial['items'])
+            return jsonify({"iniciais atualizada": inicial}), 200
+
+    return jsonify({"erro": "Iniciais não encontrados"}), 404
 
 
 if __name__ == "__main__":
